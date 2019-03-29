894c5fd11f5a214b02a727fa9d31721cce4902a4
Alexey Kushnarov <alexey.kushnarov@ukr.net>
2019-03-29 21:32:02 +0200

    prepared for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 80;
@@ -19,11 +20,11 @@ const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose
-    .connect(db, { useNewUrlParser: true })
+    .connect(db, {useNewUrlParser: true})
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
 
-app.listen(port,() => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
 
 // Passport middleware
 app.use(passport.initialize());
@@ -32,6 +33,15 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 
 //Use routes
-app.use("/api/users",users);
-app.use("/api/profile",profile);
-app.use("/api/posts",posts);
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    })
+}
