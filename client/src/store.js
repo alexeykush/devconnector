7cd390430dd27b9d0115c40a674cfc3f0850be6d
Alexey Kushnarov <alexey.kushnarov@ukr.net>
2019-05-27 12:00:11 +0300

    store fixes

--- client/src/store.js
+++ client/src/store.js
@@ -4,15 +4,28 @@ import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
 const initialState = {};
-const middleware = [thunk];
+const middlewares = [thunk];
 
-const store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-);
+let store = null;
+
+if (window.navigator.userAgent.includes("Chrome") && process.env.NODE_ENV === "development") {
+    store = createStore(
+        rootReducer,
+        initialState,
+        compose(
+            applyMiddleware(...middlewares),
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
+    );
+} else {
+    store = createStore(
+        rootReducer,
+        initialState,
+        compose(
+            applyMiddleware(...middlewares)
+        )
+    );
+}
+
 
 export default store;
